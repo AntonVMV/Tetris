731cc37dc0071566658a77287bdaf684452868db
AntonVMV <nogdire@gmail.com>
2022-06-20 20:07:17 +0300

    Updates

--- helpers/gameHelpers.ts
+++
@@ -1,7 +0,0 @@
-import { FigureModels, FiguresTypes } from "../models/MainModel";
-
-export const getRandomFigure = () => {
-  const figures = Object.keys(FigureModels) as FiguresTypes[];
-  const random = Math.floor(Math.random() * figures.length);
-  return figures[random];
-};
